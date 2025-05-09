d052430ade49b3cde5eae6f1e21074b6e67023a1
zohan <johanrafelrbrgmail.com>
2025-05-09 12:12:36 -0500

    terminado

--- script.js
+++ script.js
@@ -7,3 +7,76 @@ img.addEventListener("mouseenter", () => {
 img.addEventListener("mouseleave", () => {
   img.src = "./assets/obras/los comedores de patatas.jfif";
 });
+
+
+
+const img2 = document.getElementById("img2");
+
+img2.addEventListener("mouseenter", () => {
+  img2.src = "./assets/obras/segunda imagen (2).png";
+});
+
+img2.addEventListener("mouseleave", () => {
+  img2.src = "./assets/obras/el café de noche de Van Gogh.jfif";
+});
+const img3 = document.getElementById("img3");
+
+img3.addEventListener("mouseenter", () => {
+  img3.src = "./assets/obras/tercera imagen.png";
+});
+
+img3.addEventListener("mouseleave", () => {
+  img3.src = "./assets/obras/terraza de café por la noche_.jfif";
+});
+
+const img4 = document.getElementById("img4");
+
+img4.addEventListener("mouseenter", () => {
+  img4.src = "./assets/obras/cuarta img.png";
+});
+
+img4.addEventListener("mouseleave", () => {
+  img4.src = "./assets/obras/La belleza del día_ “El dormitorio en Arlés”, de Vincent van Gogh.jfif";
+});
+
+const img5 = document.getElementById("img5");
+
+img5.addEventListener("mouseenter", () => {
+  img5.src = "./assets/obras/quinta img.png";
+});
+
+img5.addEventListener("mouseleave", () => {
+  img5.src = "./assets/obras/girasoles.jfif";
+});
+
+const img6 = document.getElementById("img6");
+
+img6.addEventListener("mouseenter", () => {
+  img6.src = "./assets/obras/sexta img.png";
+});
+
+img6.addEventListener("mouseleave", () => {
+  img6.src = "./assets/obras/la noche estrellada Vincent van Gogh.jfif";
+});
+
+const img7 = document.getElementById("img7");
+
+img7.addEventListener("mouseenter", () => {
+  img7.src = "./assets/obras/septimaimg.png";
+});
+
+img7.addEventListener("mouseleave", () => {
+  img7.src = "./assets/obras/la ronda de los presos.jfif";
+});
+
+const img8 = document.getElementById("img8");
+
+img8.addEventListener("mouseenter", () => {
+  img8.src = "./assets/obras/ultimaimg.png";
+});
+
+img8.addEventListener("mouseleave", () => {
+  img8.src = "./assets/obras/el doctor Paul Gachet - Wikipedia, la enciclopedia libre.jfif";
+});
+
+
